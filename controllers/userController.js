1332755cecb04ba418e2f462d71d62dd35e94312
Francesco Coniglione <122475578+fconiglione@users.noreply.github.com>
2025-10-01 12:53:26 -0400

    Added login functionality

--- controllers/userController.js
+++ controllers/userController.js
@@ -37,3 +37,36 @@ export const registerUser = async (req, res) => {
         res.status(500).json({ message: 'Server error' });
     }
 };
+
+// Login user
+
+export const loginUser = async (req, res) => {
+    // Validate input
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+
+    try {
+        // Find user by email
+        const user = await User.findByEmail(email);
+        if (!user) {
+            return res.status(400).json({ message: 'Invalid credentials' });
+        }
+
+        // Compare passwords
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(400).json({ message: 'Invalid credentials' });
+        }
+
+        const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_EXPIRATION });
+
+        res.status(200).json({ message: 'Login successful', token });
+    } catch (error) {
+        console.error('Error logging in user:', error);
+        res.status(500).json({ message: 'Server error' });
+    }
+};
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { registerUser } from '../controllers/userController.js';
+import { registerUser, loginUser } from '../controllers/userController.js';
 import { body } from 'express-validator';
 
 const router = express.Router();
@@ -8,7 +8,7 @@ const router = express.Router();
 router.post('/register', [body('email').isEmail().withMessage('Valid email address required'), body('password').isLength({ min: 5 }).withMessage('Password must be at least 8 characters'), body('name').notEmpty().withMessage('Name is required')], registerUser);
 
 // Login route
-// router.post('/login', [body('email').isEmail().withMessage('Valid email address required'), body('password').isLength({ min: 5 }).withMessage('Password must be at least 8 characters')], loginUser);
+router.post('/login', [body('email').isEmail().withMessage('Valid email address required'), body('password').isLength({ min: 5 }).withMessage('Password must be at least 8 characters')], loginUser);
 
 // Logout route
 // router.post('/logout', [body('email').isEmail().withMessage('Valid email address required'), body('password').isLength({ min: 5 }).withMessage('Password must be at least 8 characters')], logoutUser);
