930a90bc0f670f1f5e6c64fd413257de3c4ff235
Francesco Coniglione <122475578+fconiglione@users.noreply.github.com>
2025-10-15 18:56:58 -0400

    fixed insightController design

--- controllers/insightController.js
+++ controllers/insightController.js
@@ -4,8 +4,8 @@ import { body, validationResult } from 'express-validator';
 
 dotenv.config();
 
-// Retrieve message from client and send to data api
-export const receiveMessage = async (req, res) => {
+// Handle message from client and send to data api then return response back to client
+export const handleMessage = async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
@@ -20,25 +20,6 @@ export const receiveMessage = async (req, res) => {
         res.status(200).json({ message: 'Message received successfully' });
     } catch (error) {
         console.error('Error processing message:', error);
-        res.status(500).json({ message: 'Server error' });
-    }
-};
-
-// Retrieve message from data api and send to client (alongside net worth data)
-export const sendMessage = async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
-    }
-    
-    const { message } = req.body;
-    const userId = req.user.id;
-    try {
-        console.log(`Sending message to user ${userId}: ${message}`);
-
-        res.status(200).json({ message: 'Message sent successfully' });
-    } catch (error) {
-        console.error('Error sending message:', error);
         res.status(500).json({ message: 'Server error' });
     }
 };
--- routes/insightRoutes.js
+++ routes/insightRoutes.js
@@ -1,18 +1,13 @@
 import express from 'express';
-import { receiveMessage, sendMessage } from '../controllers/insightController.js';
+import { handleMessage } from '../controllers/insightController.js';
 import { body } from 'express-validator';
 import { verifyUser } from '../controllers/userController.js';
 
 const router = express.Router();
 
-// Receive Message route (PROTECTED)
-router.post('/receive', verifyUser, [
+// Handle Message route (PROTECTED)
+router.post('/', verifyUser, [
     body('message').notEmpty().withMessage('Message is required')
-], receiveMessage);
-
-// Send Message route (PROTECTED)
-router.post('/send', verifyUser, [
-    body('message').notEmpty().withMessage('Message is required')
-], sendMessage);
+], handleMessage);
 
 export default router;
