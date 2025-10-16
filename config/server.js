cc80ed59b20052ce1267ed4a0f9e6fca54fa3b18
Francesco Coniglione <122475578+fconiglione@users.noreply.github.com>
2025-10-16 12:13:27 -0400

    fixed cors issue

--- config/server.js
+++ config/server.js
@@ -13,7 +13,7 @@ const app = express();
 
 // Middleware setup
 app.use(cors({
-    origin: [process.env.CORS_DOMAIN],
+    origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
 }));
